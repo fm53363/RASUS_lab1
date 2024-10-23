393b601400692dbb81a321901ec3172629990b67
fmaznik <fran.maznik@gmail.com>
2024-10-23 21:53:14 +0200

    add unique constraint to Sensor

--- server/src/main/java/hr/fer/tel/rassus/server/beans/Sensor.java
+++ server/src/main/java/hr/fer/tel/rassus/server/beans/Sensor.java
@@ -3,14 +3,20 @@ package hr.fer.tel.rassus.server.beans;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.net.URI;
 import java.util.List;
 
 @Entity
 @Data
+@Table(name = "sensor", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"ip", "port"})
+})
 public class Sensor {
     @Id
     @GeneratedValue
     private Long id;
+
+
     private Double latitude;
     private Double longitude;
     private String ip;
@@ -18,6 +24,12 @@ public class Sensor {
 
     @OneToMany(mappedBy = "sensor")
     List<Reading> readings;
+
+    public URI getUrl() {
+        // Construct the URL based on the IP address and port
+        return URI.create("http://" + ip + ":" + port); // Assuming getId() gives the unique identifier
+
+    }
 
 
 }
