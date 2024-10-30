9559b86809175c4431e780473dffe7b579746b38
fmaznik <fran.maznik@gmail.com>
2024-10-30 21:05:23 +0100

    server  - add no2 field to Reading

--- server/src/main/java/hr/fer/tel/rassus/server/entity/Reading.java
+++ server/src/main/java/hr/fer/tel/rassus/server/entity/Reading.java
@@ -1,22 +1,28 @@
 package hr.fer.tel.rassus.server.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Entity
 @Data
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Reading {
     @Id
     @GeneratedValue
     private Long id;
-
-
+    @NotNull
     private Double temperature;
+    @NotNull
     private Double pressure;
+    @NotNull
     private Double humidity;
+
     private Double co;
+    private Double no2;
     private Double so2;
 
     @ManyToOne
